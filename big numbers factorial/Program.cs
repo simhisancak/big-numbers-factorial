6d3a99a5116e35aec7a22433bbbae2c6826aa2e0
Simhi Sancak <hasan_71_99@hotmail.com>
2019-10-06 00:35:46 +0300

    Proje dosyası ekle.

---
+++ big numbers factorial/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Numerics;
+using System.IO;
+
+namespace big_numbers_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        basadon:
+            string[] simhi = new string[6];
+            Stopwatch watch = new Stopwatch();
+            BigInteger ss = 0;
+
+            Console.Clear();
+            Console.Write("Lütfen Sayıyı Giriniz: ");
+            
+            long s = Convert.ToInt64( Console.ReadLine());
+            
+            watch.Start();
+           
+            ss = calculate.Factorial(s);
+
+            simhi[0] = ("Simhi Sancak");
+            simhi[1] = ("İşlem Tarihi: " + DateTime.Now.ToString());
+            simhi[2] = ("Girilen Sayı: " + s);
+            simhi[4] = ("Sonuç: " + Convert.ToString(ss));
+            string masaustu_dizin = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + "factorial log" + ".txt";
+            if (!System.IO.File.Exists(masaustu_dizin))
+            {
+                FileStream fs = File.Create(masaustu_dizin);
+                fs.Close();
+                System.IO.File.WriteAllLines(masaustu_dizin, simhi);
+            }
+            else
+            {
+                System.IO.File.WriteAllLines(masaustu_dizin, simhi);
+            }
+            
+            watch.Stop();
+            
+
+            TimeSpan ts = watch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds );
+
+
+            Console.Clear();
+            Console.WriteLine("-------------Bitti-------------");
+            Console.WriteLine();
+            Console.WriteLine("Girilen Sayı: " + s);
+            Console.WriteLine();
+            Console.WriteLine("İşlem Süresi: " + elapsedTime);
+            Console.WriteLine();
+            Console.WriteLine("Dosya Yolu: " + masaustu_dizin);
+            Console.WriteLine();
+            Console.WriteLine("-------------Bitti-------------");
+            Console.WriteLine();
+            Console.WriteLine("Başa Dönmek İçin Herhangi Bir Tuşa Bas veya Çıkmak İçin ESC Tuşuna Bas");
+           
+            
+            while (true)
+            {
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                else
+                {
+                    goto basadon;
+                }
+            }
+        }
+    }
+}
---
+++ big numbers factorial/calculate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Diagnostics;
+using System.IO;
+
+namespace big_numbers_factorial
+{
+    class  calculate
+    {
+        static readonly int DegreeOfParallelism = Environment.ProcessorCount;
+        
+        public static BigInteger Factorial(long x)
+        {
+            Console.Clear();
+            Console.WriteLine("Lütfen Bekleyiniz...");
+            var parallelTasks =
+                Enumerable.Range(1, DegreeOfParallelism).Select(i => Task.Factory.StartNew(() => Multiply(x, i),TaskCreationOptions.LongRunning)).ToArray();
+
+            Task.WaitAll(parallelTasks);
+
+            BigInteger finalResult = 1;
+
+            foreach (var partialResult in parallelTasks.Select(t => t.Result))
+            {
+                
+                finalResult *= partialResult;
+            }
+            
+            return finalResult;
+
+        }
+
+
+        public static BigInteger Multiply(long upperBound, int startFrom)
+        {
+            BigInteger result = 1;
+
+            for (var i = startFrom; i <= upperBound; i += DegreeOfParallelism)
+                result *= i;
+            return result;
+        }
+       
+    }
+}
